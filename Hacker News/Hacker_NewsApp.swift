447fda6c4e4ae2e56b8967a3692acfa896075db8
mohammadgdz <mhd.gdz@gmail.com>
2023-08-17 10:48:56 +0330

    Initial Commit

---
+++ Hacker News/Hacker_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hacker_NewsApp.swift
+//  Hacker News
+//
+//  Created by Mohammad on 2023-08-17.
+//
+
+import SwiftUI
+
+@main
+struct Hacker_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
