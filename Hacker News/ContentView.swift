5ddb54f02f290e612012445a660bc5d9e9448dbb
mohammadgdz <mhd.gdz@gmail.com>
2023-08-17 11:00:20 +0330

    add dynamic list

--- Hacker News/ContentView.swift
+++ Hacker News/ContentView.swift
@@ -9,15 +9,26 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack{
+            List(posts){ post in
+                Text(post.title)
+            }
+            .navigationTitle("Hacker News")
         }
-        .padding()
     }
 }
+
+
+struct Post: Identifiable {
+    let id : String
+    let title : String
+}
+
+let posts = [
+    Post(id: "1", title: "Hello"),
+    Post(id: "2", title: "Bonjour"),
+    Post(id: "3", title: "Hola"),
+]
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
